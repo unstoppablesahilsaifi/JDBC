16c0b4c43005b6221656950e7a38d6b770591f82
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2022-08-08 20:18:50 +0530

    Update JDBC_Tuts.java

--- JDBC_Tuts.java
+++ JDBC_Tuts.java
@@ -1,1 +1,13 @@
-// Tests
+/* From Learn code with Durgesh.
+JDBC- It is an standared API provided by oracle for java applications to interact with differ types of databases.
+
+How JDBC Works-
+
+              =======>>                                 JDBC DB Drivers                           Databases
+  Java App    <<=======        JDBC API                    Oracle  DB                              Oracle
+  >-------JDBC application layer------<                      SQL DB                                 SQL
+                                                        >------------JDBC Driver Layer-------------------<
+  >------------------------------------JDBC Architecture-------------------------------------------------<
+  
+  To concept ye h ki hmare paas java application h or hmee data store krna h DB m, mtlb query likhne k liye hme JDBC API ki need pdegi ko connectivity k liye drivers ki
+  jisse ki hmare app ki query drivers k through connect hokr Database s interact kr skeee.
