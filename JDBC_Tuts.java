bb44dec31db206ffeef0a934cd86a75bc9ae94f2
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2022-08-10 22:41:49 +0530

    Update JDBC_Tuts.java

--- JDBC_Tuts.java
+++ JDBC_Tuts.java
@@ -23,6 +23,38 @@ How JDBC Works-
    
    3. Now we are using mysql to hme mysql ka driver chahiye iske driver ko bolte h mysql-connector.
      Please download the same version connector same as the verison of mysql.
-     To check the version of mysql-> open mysql client view-> type SHOW VARIABLES LIKE "%version%";
+     To check the version of mysql-> open mysql client view-> type 
+     // Basically jo driver hote h vo jar file hoti h 
+     
+     Ab Scene h ki driver download krke C drive m paste krdo jar file ko uske baad path copy kro jar file tak ka, environment variable m jake classpath name k varible
+     bnakr paste krdo.
+     uske baad Open CMD- write cls and enter ans write javap com.mysql.jdbc.Driver and Enter. All Done Yeahhhhhhhhhhhhhhhhh..............*/
+     
+ ++++++++++++++++++++++++++++++++++
+ //Now steps to connect database;
+ 
+ 1. Load the Driver:
+                  //Java m ek Class naam ki ek class h jisme ek method h forName() is method ko hm koi bhi class string ki format m de to vo usee memory m load krdeta h.
+                   Class forName("com.mysql.jdbc.Driver");
+                                                                  or
+                 //Dusra tarika bhi h, ek class h DriverManager jiske paas ek method h registerDriver jo ki hmare driver ka object leta h;
+                 DriverManager registerDrive(new com.mysql.jdbc.Driver());
+
+2.Create a connection:
+                  Connection con=DriverManager.getConnection();
+                // DriverManager jo class h uska getconnection jo method h vo hmee hmre connection ka method de dega to hme Connection type k hi variable m store krna pdega.
+                Note: Java.sql ko import bhi krna pdega.
+                  Connection con=DriverManager.getConnection("url","username","password");
+                 // getConnection method 3 cheeje leta h ek to url(DB h kha pe install, IP bhi dena hoga computer ka) uske baad username and password.
+                  Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/dbname","root","root");
+                 To check the port number u'll see port number.
+                   
+3.Create Query:
+               
+               
+
+
+
+
      
    
